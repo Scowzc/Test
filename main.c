ea7cda8b352bbd637af592519d498bdc74fcbc5f
Scowzc <244034529@qq.com>
2021-04-08 13:52:58 +0800

    4.8

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-#include"jjcc.h"
+#include<jjcc.h>
 int main() {
     int a,b;
    	scanf("%d%d",&a,&b);
     printf("%d + %d = %d\n",a,b,add(a,b));
- 	printf("%d - %d = %d\n",a,b,sub(a,b));
-	printf("%d * %d = %d\n",a,b,mul(a,b));
+ 	  printf("%d - %d = %d\n",a,b,sub(a,b));
+	  printf("%d * %d = %d\n",a,b,mul(a,b));
     printf("%d / %d = %d\n",a,b,div(a,b));
     return 0;
 }
